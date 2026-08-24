09d09922f311214bdec98441ade77b2fe8009e62
Huayu Zhang <zhanghuayu.dev@gmail.com>
2026-08-24 21:00:37 +0800

    multithreading finished

---
+++ threading/1-add-thread.py
@@ -0,0 +1,35 @@
+import threading
+import time
+
+
+def thread_job():
+    print(f"this is an added thread, number is {threading.current_thread()}")
+    for i in range(10):
+        time.sleep(0.1)
+    print(f"{threading.current_thread()} finished")
+
+
+def T2_job():
+    print(f"{threading.current_thread()} started")
+    print(f"{threading.current_thread()} finished")
+
+
+def main():
+    added_thread = threading.Thread(target=thread_job, name="T1")
+    thread2 = threading.Thread(target=T2_job, name="T2")
+
+    added_thread.start()
+    thread2.start()
+
+    added_thread.join()
+    thread2.join()
+
+    print("all done")
+
+    # print(threading.active_count())
+    # print(threading.enumerate())
+    # print(threading.current_thread())
+
+
+if __name__ == "__main__":
+    main()
